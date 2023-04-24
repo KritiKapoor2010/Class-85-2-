46c64d632ea5e3f82d8fdccb98d41e1ba58df4e1
KritiKapoor2010 <98330511+KritiKapoor2010@users.noreply.github.com>
2023-04-24 18:52:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Class-85-2-
+# PRO-C85-boilerplate
+
---
+++ config.js
@@ -0,0 +1,16 @@
+export 
+const firebaseConfig = {
+    apiKey: "AIzaSyAuoec660AhGwV3dHCOt7EDIU0sydBh2fA",
+    authDomain: "story-telling-app-c0edc.firebaseapp.com",
+    projectId: "story-telling-app-c0edc",
+    storageBucket: "story-telling-app-c0edc.appspot.com",
+    messagingSenderId: "96201177529",
+    appId: "1:96201177529:web:ce258f54ac522b6486de68"
+  };
+
+
+
+
+
+
+
